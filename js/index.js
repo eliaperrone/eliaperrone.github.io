d482d15e754df98a64c804c80089f1066c7b48ed
eliaperrone <perrone.elia@outlook.com>
2019-10-08 14:07:49 +0200

    add protect password for cv

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function passwd() {
-    var password = prompt('Enter the password to download the file:');
+    var password = prompt('Ask password to perrone.elia@outlook.com and after enter the password to download the file:');
     if (password == "DWcvEP2019") {
         window.open("./doc/CV_nuovo.pdf")
     } else {
