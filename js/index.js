99219b78c32ffc0a3922bdf50bbc404af779d037
Elia Perrone <eliaperrone@MacBook-Pro-di-Elia.local>
2020-01-14 11:00:42 +0100

    update cv and pass

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function passwd() {
     var password = prompt('Ask password to perrone.elia@outlook.com and after enter the password to download the file:');
-    if (password == "DWcvEP2019") {
-        window.open("./doc/CV_nuovo.pdf")
+    if (password == "DWcvEP2020") {
+        window.open("./doc/CV_EP.pdf")
     } else {
         alert("incorrect password, please try again");
     }
